40b27af8aa29c256e5cb0bd0c8bca4d7d5a08131
Nik Visenko <nik@nested.com>
2022-05-30 20:32:35 +0100

    test this thing

--- src/tests/integration/customers.test.ts
+++ src/tests/integration/customers.test.ts
@@ -67,6 +67,83 @@ describe("GET /customers/:id", () => {
       });
   });
 
+  describe("POST /customers", () => {
+    it("returns created customer if payload passes validation", async () => {
+      const payload = {
+        givenName: "Agent",
+        familyName: "Smith",
+        email: "evil@matrix.com",
+      };
+
+      const app = supertest(await App.initApp());
+      await app
+        .post(`/customers`)
+        .send(payload)
+        .expect(201)
+        .then(async (response) => {
+          expect(response.body.data.givenName).toBe(payload.givenName);
+          expect(response.body.data.familyName).toBe(payload.familyName);
+          expect(response.body.data.email).toBe(payload.email);
+          expect(response.body.success).toBe(true);
+          const customer: CustomerInstance | null = await DB.Customers.findByPk(
+            response.body.id,
+          );
+
+          if (customer) {
+            const email = customer.getDataValue("email");
+            const givenName = customer.getDataValue("givenName");
+            const familyName = customer.getDataValue("familyName");
+            expect(response.body.data.email).toBe(email);
+            expect(response.body.data.familyName).toBe(familyName);
+            expect(response.body.data.givenName).toBe(givenName);
+          }
+        });
+    });
+
+    it("returns errors if payload fails validation", async () => {
+      const payload = {
+        familyName: "Smith1122",
+        email: "evilmatrix.com",
+      };
+
+      const app = supertest(await App.initApp());
+      await app
+        .post(`/customers`)
+        .send(payload)
+        // .expect(400)
+        .then(async (response) => {
+          const errors = [
+            {
+              value: "Smith1122",
+              msg: "Please use alpha characters only",
+              param: "familyName",
+              location: "body",
+            },
+            {
+              msg: "Given name cannot be blank",
+              param: "givenName",
+              location: "body",
+            },
+            {
+              value: "@evilmatrix.com",
+              msg: "Please provide valid email address",
+              param: "email",
+              location: "body",
+            },
+          ];
+
+          expect(JSON.stringify(response.body.errors)).toBe(
+            JSON.stringify(errors),
+          );
+        });
+    });
+
+    it("returns 404 if param is not uuid", async () => {
+      const app = supertest(await App.initApp());
+      await app.get(`/customers/not-an-id`).expect(404);
+    });
+  });
+
   it("throws an exception if id is null", async () => {
     const id = null;
 
--- src/tests/unit/services/customers.service.test.ts
+++ src/tests/unit/services/customers.service.test.ts
@@ -2,6 +2,7 @@ import supertest from "supertest";
 import { customerService } from "@src/services/customers.service";
 import { App } from "@src/app";
 import { DB } from "@src/db/index";
+import { CreateCustomerDto } from "@src/validators/createCustomer.validator";
 
 afterEach(() => {
   jest.clearAllMocks();
@@ -55,6 +56,29 @@ describe("customerService", () => {
       .expect(200)
       .then((response) => {
         expect(response.body.data).toHaveLength(3);
+      });
+  });
+
+  it("createCustomer responds with customers data", async () => {
+    const customerData: CreateCustomerDto = {
+      email: "wtf@email.com",
+      givenName: "Aaaa",
+      familyName: "Citrus",
+    };
+    DB.Customers.findOne = jest.fn().mockReturnValue(null);
+    customerService.createCustomer = jest.fn().mockReturnValue({
+      id: "ca48570d-6265-453a-b9da-ca9bc982bfee",
+      ...customerData,
+    });
+    const app = supertest(await App.initApp());
+    app
+      .post("/customers")
+      .send(customerData)
+      .expect(201)
+      .then((response) => {
+        expect(response.body.data.id).toBe(
+          "ca48570d-6265-453a-b9da-ca9bc982bfee",
+        );
       });
   });
 });
